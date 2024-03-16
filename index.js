1b095c58bd1164ab60c751d1ae9632d1cf156011
Nityam24 <nityampatel2402@gmail.com>
2024-03-16 14:38:03 +0530

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-//practice project for learning node.js
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
